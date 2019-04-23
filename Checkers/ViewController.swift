29f7e84f8015db8808330cbaf2f31b9247f82163
Trevor Moore <moore.trev@tamu.edu>
2019-04-23 14:49:12 -0500

    Blank iOS Project Intial Commit

---
+++ Checkers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checkers
+//
+//  Created by Trevor Moore on 4/23/19.
+//  Copyright © 2019 Trevor Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
